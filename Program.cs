304b13d6ccce15fe2d179ece8eada37b8313f119
Shohboz Xurramov <shohboztuit@gmail.com>
2023-11-07 09:24:49 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ranglar uchun enum yarating hamda quyidagicha
+            //informatsiyalarni console ga chiqaring.
+            //RED at index 0
+            //GREEN at index 1
+            //BLUE at index 2
+            foreach (var item in Enum.GetValues(typeof(Ranglar)))
+            {
+                Console.WriteLine(item + " at index " + (int)item);
+            }
+        }
+    }
+    enum Ranglar
+    {
+        RED = 0,
+        GREEN = 1,
+        BLUE = 2,
+        ORANGE = 3,
+        WHITE = 4,
+    }
+}
